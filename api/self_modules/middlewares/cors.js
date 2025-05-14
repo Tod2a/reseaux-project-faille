6acfc30b45c5b7e34b2d6086ff63219bd4176440
tod2a <t.copin@yahoo.fr>
2025-05-14 20:31:40 +0200

    test cors

--- api/self_modules/middlewares/cors.js
+++ api/self_modules/middlewares/cors.js
@@ -1,9 +1,9 @@
-var whitelist = ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001']
+var whitelist = ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001', 'https://reseaux-project-faille-app.onrender.com']
 
 module.exports = corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
-      let corsOption = {origin: true}
+      let corsOption = { origin: true }
       callback(null, corsOption)
     } else {
       callback(new Error('Not allowed by CORS'))
