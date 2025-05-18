e8746e69c318d9c32042e56d6987a22e09d55a41
tod2a <t.copin@yahoo.fr>
2025-05-18 18:10:30 +0200

    test resolve CORS error

--- api/self_modules/middlewares/cors.js
+++ api/self_modules/middlewares/cors.js
@@ -1,12 +1,19 @@
-var whitelist = ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001', 'https://reseaux-project-faille-app.onrender.com']
+const whitelist = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://reseaux-project-faille-app.onrender.com'
+];
 
-module.exports = corsOptions = {
+const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
-      let corsOption = { origin: true }
-      callback(null, corsOption)
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
-  }
-}
+  },
+  credentials: true,
+};
+
+module.exports = corsOptions;
